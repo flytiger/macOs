d7022e96eec67d7af598706d0f3b3a3ceb0e029e
Joe <flytiger@yahoo.com>
2018-06-14 22:45:43 -0700

    add install script, more git aliases

---
+++ home/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -snf $PWD/.bashrc $HOME/.bashrc
+ln -snf $PWD/.bash_profile $HOME/.bash_profile
+ln -snf $PWD/.bash_aliases $HOME/.bash_aliases
+ln -snf $PWD/.bash_exports $HOME/.bash_exports
+ln -snf $PWD/.bash_functions $HOME/.bash_functions
+ln -snf $PWD/.gitconfig $HOME/.gitconfig
